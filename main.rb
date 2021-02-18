b6340aa15edf3eaefd0f2303d34368cd6935793e
Hiroto-Iizuka <oroxith@gmail.com>
2021-02-18 21:48:10 +0900

    誤記述箇所を修正

--- main.rb
+++ main.rb
@@ -21,9 +21,8 @@ class Pokemon
 
     if attack_num == 0
       puts "急所にあたった！"
-      damage = calculate_special_attack - enemy.defense
+      damage = calculate_critical_hit - enemy.defense
     else
-      puts "通常攻撃"
       damage = @offense - enemy.defense
     end
 
@@ -33,7 +32,7 @@ class Pokemon
     puts "#{enemy.name}の残りHPは#{enemy.hp}だ"
   end
 
-  def calculate_special_attack
+  def calculate_critical_hit
     @offense * 1.5
   end
 
@@ -51,17 +50,19 @@ class Enemy
     @defense = params[:defense]
   end
 
+  def move_name(selected_move)
+    puts "#{@name}の#{selected_move.name}!!"
+  end
+
   # 攻撃
   def attack(pokemon)
-    puts "#{@name}の[技名]"
 
     attack_num = rand(9)
 
     if attack_num == 0
       puts "急所にあたった！"
-      damage = calculate_special_attack - pokemon.defense
+      damage = calculate_critical_hit - pokemon.defense
     else
-      puts "通常攻撃"
       damage = @offense - pokemon.defense
     end
     pokemon.hp -= damage
@@ -70,7 +71,7 @@ class Enemy
     puts "#{pokemon.name}の残りHPは#{pokemon.hp}だ"
   end
 
-  def calculate_special_attack
+  def calculate_critical_hit
     @offense * 1.5
   end
 
@@ -88,14 +89,14 @@ end
 
 class Trainer
   attr_reader :moves, :selected_move
-  def initialize(move_paramas)
+  def initialize(move_params)
     @moves = []
-    register_move(move_paramas)
+    register_move(move_params)
   end
 
   # 技を登録
-  def register_move(move_paramas)
-    move_paramas.each do |param|
+  def register_move(move_params)
+    move_params.each do |param|
       @moves << Move.new(param)
     end
   end
@@ -121,6 +122,40 @@ class Trainer
   end
 end
 
+
+class EnemyMove
+  attr_reader :id, :name, :type
+  @@count = 0
+  def initialize(enemy_move_params)
+    @id = @@count += 1
+    @name = enemy_move_params[:name]
+    @type = enemy_move_params[:type]
+  end
+end
+
+class EnemyAction
+  attr_reader :enemymoves, :selected_move
+  def initialize(enemy_move_params)
+    @enemymoves = []
+    register_move(enemy_move_params)
+  end
+
+  # 技を登録
+  def register_move(enemy_move_params)
+    enemy_move_params.each do |param|
+      @enemymoves << EnemyMove.new(param)
+    end
+  end
+
+  # 自動で技の選択
+  def select_move(enemymoves)
+    select_move_num = rand(1..2)
+    @selected_move = enemymoves.find{|move| move.id == select_move_num}
+    enemymoves[select_move_num - 1]
+  end
+end
+
+
 # ポケモンインスタンス生成
 pokemon = Pokemon.new(name: "ヒトカゲ", type: "ほのお", hp: 300, offense: 100, defense: 50)
 enemy = Enemy.new(name: "ゼニガメ", type: "みず", hp: 400, offense: 70, defense: 80)
@@ -131,6 +166,12 @@ move_params = [
     {name: "ひのこ", type: "ほのお"}
 ]
 
+# 敵の技データ
+enemy_move_params = [
+  {name: "たいあたり", type: "ノーマル"},
+  {name: "みずでっぽう", type: "みず"}
+]
+
 # 技インスタンス生成
 trainer = Trainer.new(move_params)
 trainer.disp_moves
@@ -138,4 +179,7 @@ trainer.select_move(trainer.moves)
 pokemon.move_name(trainer.selected_move)
 pokemon.attack(enemy)
 
-#enemy.attack(pokemon)
+enemyaction = EnemyAction.new(enemy_move_params)
+enemyaction.select_move(enemyaction.enemymoves)
+enemy.move_name(enemyaction.selected_move)
+enemy.attack(pokemon)
